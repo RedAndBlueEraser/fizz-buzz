b5c24d82ac8e6bb5c4bcc6e889f79b4a87c55e70
Harry <RedAndBlueEraser@outlook.com>
2016-09-15 21:17:38 +1000

    Added boilerplate Java program.

---
+++ fizz-buzz.java
@@ -0,0 +1,16 @@
+/*
+ * fizz-buzz.java
+ * Version 20160915
+ * Written by Harry Wong (RedAndBlueEraser)
+ */
+
+public class FizzBuzz {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+}
