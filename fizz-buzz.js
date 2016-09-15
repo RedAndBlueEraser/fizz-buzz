476dfb5b5f10f2ba145a268c6468121c29dbf1bd
Harry <RedAndBlueEraser@outlook.com>
2016-09-15 21:12:42 +1000

    Completed fizz buzz JavaScript program.

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -4,4 +4,38 @@
  * Written by Harry Wong (RedAndBlueEraser)
  */
 
-alert("Hello, World!");
+function getFizzBuzz(length) {
+    var i,              // Iteration over fizz buzz sequence.
+        fizzBuzz = '';  // Fizz buzz sequence string.
+
+    // Iterate over fizz buzz sequence.
+    for (i = 1; i <= length; i++) {
+        // Iteration is divisible by 3; add "Fizz".
+        if (i % 3 === 0) {
+            fizzBuzz += 'Fizz';
+            // Iteration is divisible by 3 and 5; add "Buzz" after "Fizz".
+            if (i % 5 === 0) {
+                fizzBuzz += ' Buzz';
+            }
+        }
+
+        // Iteration is divisible by 5; add "Buzz".
+        else if (i % 5 === 0) {
+            fizzBuzz += 'Buzz';
+        }
+
+        // Add iteration.
+        else {
+            fizzBuzz += i;
+        }
+
+        // Separate values with a comma except for last value.
+        if (i != length) {
+            fizzBuzz += ', ';
+        }
+    }
+
+    return fizzBuzz;
+}
+
+alert(getFizzBuzz(50));
