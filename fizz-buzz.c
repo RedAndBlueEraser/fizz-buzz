39ba7c170f202d95384178b82d54a26f02340ea5
Harry <RedAndBlueEraser@outlook.com>
2016-09-15 21:05:28 +1000

    Initial commit with boilerplate C program.

---
+++ fizz-buzz.c
@@ -0,0 +1,12 @@
+/*
+ * fizz-buzz.c
+ * Version 20160915
+ * Written by Harry Wong (RedAndBlueEraser)
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    printf("Hello, World!\n");
+    return 0;
+}
